07ab1a82e4fda4e79ee8d259ff6ba9c381105113
Andrew Rimanic <Andrew Rimanic>
2017-07-12 18:30:22 -0700

    start sonar interrupts

--- Main.ino
+++ Main.ino
@@ -17,15 +17,20 @@ int dCon = 0;
 
 // TIMING VARIABLE
 int timeElapsed = 0;
+
+// Sonar interrupt variables
+bool sonarInterrupt;
+String offEdgeTurn;
+
 void setup()
 {
 #include <phys253setup.txt>
-LCD.clear();
+  LCD.clear();
   Serial.begin(9600) ;
   enableExternalInterrupt(INT1, CHANGE);
   enableExternalInterrupt(INT2, CHANGE);
   enableExternalInterrupt(INT3, RISING);
-  attachTimerInterrupt(1, timer1ISR);
+  attachTimerInterrupt(60000, timer1ISR);
   printCount = 0;
 }
 
@@ -49,57 +54,75 @@ void loop() { // !!! final version. working as intended do not modify
   /////////////////////////////
   ///// Robot AI //////////////
   /////////////////////////////
-
   if (startbutton()) {
-    attachISR(INT1, ISR1);
-    attachISR(INT2, ISR2);
-    attachISR(INT3, ISR3);
-    timeElapsed = 0;
     phase1();
   }
 }
 
 void phase1() { //!!! being used for testing functions right now
+  // Phase 1 setup
+  attachISR(INT1, ISR1);
+  attachISR(INT2, ISR2);
+  attachISR(INT3, ISR3);
+  timeElapsed = 0;
 
-  while (1){    
-  PID4follow();
-  moveUpperArm(drivePos);
-  LCD.clear();
-  LCD.print(timeElapsed);
-  delay(500);
+  // end of setup
 
-if (stopbutton()) {
-    while (stopbutton()) {
+
+  while (1) {
+    if (sonarInterrupt) {
+      if (offEdgeTurn == "L") {
+        setMotors(0 , 255 , 0); // hard left turn
+      } else  {
+        setMotors(255 , 0 , 0); // hard right turn
+      }
+    } else {
+
+      motor.speed(0, 255);
+  motor.speed(1, 255);
+  motor.speed(2, 255);
+  motor.speed(3, 255);
+
+  delay(1000);
+  motor.speed(0, 0);
+  motor.speed(1, 0);
+  motor.speed(2, 0);
+  motor.speed(3, 0);
+  
+ //     PID4follow();
+      moveUpperArm(drivePos);
+      LCD.clear();
+      LCD.print(timeElapsed);
+      delay(500);
+
+      if (stopbutton()) {
+        while (stopbutton()) {
+        }
+        menu();
+      }
     }
-    menu();
-  }
-
   }
 }
 
-void timer1ISR(){
+void timer1ISR() {
   timeElapsed++;
 }
 
 
-void ISR1(){//!!! make this work for sonar1
-  LCD.clear();
-  LCD.print("Vitor sucks");
-  LCD.setCursor(0,1);
-  LCD.print("at foosball");
+void ISR1() { //!!! make this work for Lsonar
+  sonarInterrupt = !sonarInterrupt;
+  offEdgeTurn = "R";
 }
 
-void ISR2(){//!!! make this work for sonar2
-  LCD.clear();
-  LCD.print("Vitor sucks");
-  LCD.setCursor(0,1);
-  LCD.print("at foosball");
+void ISR2() { //!!! make this work for Rsonar
+  sonarInterrupt = !sonarInterrupt;
+  offEdgeTurn = "R";
 }
 
-void ISR3(){//!!! make this work for wheel measurement
+void ISR3() { //!!! make this work for wheel measurement
   LCD.clear();
   LCD.print("Vitor sucks");
-  LCD.setCursor(0,1);
+  LCD.setCursor(0, 1);
   LCD.print("at foosball");
 }
 
